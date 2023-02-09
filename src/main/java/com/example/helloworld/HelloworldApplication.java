15e077b221bd44f228ff924789c004e4378caaba
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-09 14:03:56 +0800

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -47,7 +47,7 @@ class HelloController {
 
   @GetMapping("/")
   String hello() {
-    return "Hello World! Welcome to ABC API Services...";
+    return "Hello World! Welcome to Commerce Dot Com API Services...";
   }
 
 }
