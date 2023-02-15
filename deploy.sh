c808e19c5cb693536c9a7f5e38944933e47597a7
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-15 16:23:23 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 1. Create two projects
+gcloud projects create frontend-reactjs --name="Frontend ReactJS"
+gcloud projects create backend-springboot --name="Backend Springboot"
+
+# Set region to asia-southeast1
+gcloud config set compute/region asia-southeast1
+
+# 2. Clone the backend-springboot and frontend-reactjs repositories
+git clone https://github.com/zipzip81/backend-springboot.git
+git clone https://github.com/zipzip81/frontend-reactjs.git
+
+# 3. Deploy the frontend-reactjs code to Firebase Hosting
+cd frontend-reactjs
+gcloud config set project frontend-reactjs
+npm install
+npm run build
+firebase login --no-localhost
+firebase init hosting
+firebase deploy
+
+# 4. Deploy the backend-springboot code to App Engine
+cd ../backend-springboot
+gcloud config set project backend-springboot
+./mvnw clean package
+gcloud app create
+gcloud app deploy
+
+echo "Script finished running."
