510a2e455392bd03832163df11032e60b8c96290
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-21 20:03:43 +0800

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,31 +18,83 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
 
 @SpringBootApplication
-@RestController
 public class HelloworldApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HelloworldApplication.class, args);
   }
-	
-  @GetMapping("/")
-  public String hello() {
-	    return "Hello World! Welcome to TEST API Services...";
-  }
-	
-  @RequestMapping("/api/v1")
-  @GetMapping("/hello/{name}")
-  public String helloV1(@PathVariable String name) {
-        String message = "Hello, " + name + "!";
-        return "{\"message\": \"" + message + "\"}";
+  
+  @Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("https://dev-frontend-reactjs.web.app");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("OPTIONS");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+  
+}
+
+
+@Component
+@Order(0)
+class AppCdsApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
+  private final boolean appcds;
+  private final ApplicationContext ctx;
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  AppCdsApplicationListener(@Value("${appcds:false}") boolean appcds,
+      ApplicationContext ctx) {
+    this.appcds = appcds;
+    this.ctx = ctx;
   }
 
-  @RequestMapping("/api/v2")
-  @GetMapping("/hello/{name}")
-  public String helloV2(@PathVariable String name) {
-        return "Welcome " + name;
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    if (appcds) {
+      restTemplate.getForEntity("http://localhost:8080/", String.class);
+      SpringApplication.exit(ctx, () -> 0);
+    }
   }
-	
+}
+
+@RestController
+class HelloController {
+  @GetMapping("/")
+  String hello() {
+    return "Hello World! Welcome to TEST API Services...";
+  }
+}
+
+
+@RestController
+@RequestMapping("/api/v1")
+class HelloControllerV1 {
+
+    @GetMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        String message = "Hello, " + name + "!";
+        return "{\"message\": \"" + message + "\"}";
+    }
+}
+
+@RestController
+@RequestMapping("/api/v2")
+class HelloControllerV2 {
+
+    @GetMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Welcome " + name;
+    }
 }
