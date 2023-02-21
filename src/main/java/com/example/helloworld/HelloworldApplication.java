6fbd82ddc2cbf7ba5391e84b3be2c0be494d51f5
zipzip81 <65789665+zipzip81@users.noreply.github.com>
2023-02-21 15:44:22 +0800

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -20,8 +20,24 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+         .allowedOrigins("*")
+         .allowedMethods("GET", "POST", "PUT", "DELETE")
+         .allowedHeaders("*")
+         .allowCredentials(false)
+         .maxAge(3600);
+   }
+}
 
 @SpringBootApplication
 public class HelloworldApplication {
